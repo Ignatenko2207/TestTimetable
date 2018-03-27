89eb04c22f01893378fb9a575b0f38ec4435afc8
yarosla8a <yaro67819@gmail.com>
2018-03-27 20:44:32 +0300

    LessonDAO

---
+++ TestTimeTable/src/main/java/org/itstep/dao/LessonDAO.java
@@ -0,0 +1,17 @@
+package org.itstep.dao;
+
+import java.util.List;
+
+import org.itstep.model.Group;
+import org.itstep.model.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+
+public interface LessonDAO extends JpaRepository<Lesson, Integer>{
+	
+	List<Lesson> findAllByStartTime(Long startTime , Long endTime );
+	List<Lesson> findAllByStartTime(Long startTime,Long endTime ,Group group );
+	
+
+}

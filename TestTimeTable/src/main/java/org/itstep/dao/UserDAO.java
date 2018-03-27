b840dc2a9a0c8db66939fff5364d33815079321c
Alex Ignatenko <alex.ignatenko@scalors.com>
2018-03-27 20:02:42 +0300

    Made UserDAO

---
+++ TestTimeTable/src/main/java/org/itstep/dao/UserDAO.java
@@ -0,0 +1,13 @@
+package org.itstep.dao;
+
+import java.util.List;
+
+import org.itstep.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserDAO extends JpaRepository<User, String>{
+
+	List<User> findAllByGroup(String groupName);
+}

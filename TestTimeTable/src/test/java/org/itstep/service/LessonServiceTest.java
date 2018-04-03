3554af8753b6c93be2201e4ce72bc5d7a59d2b74
Alex Ignatenko <ignatenko2207@gmail.com>
2018-04-03 21:03:51 +0300

    Fix test

--- TestTimeTable/src/test/java/org/itstep/service/LessonServiceTest.java
+++ TestTimeTable/src/test/java/org/itstep/service/LessonServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,7 +24,7 @@ public class LessonServiceTest {
 	@Autowired
 	LessonService lessonService;
 	
-	@Autowired
+	@MockBean
 	LessonDAO lessonDao;
 	
 	@Test
--- TestTimeTable/src/test/java/org/itstep/service/TeacherServiceTest.java
+++ TestTimeTable/src/test/java/org/itstep/service/TeacherServiceTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,7 +24,7 @@ public class TeacherServiceTest {
 	@Autowired
 	TeacherService teaherService;
 	
-	@Autowired
+	@MockBean
 	TeacherDAO teacherDao;
 	
 	@Test

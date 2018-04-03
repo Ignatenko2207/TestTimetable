952f7a98e5b6705b3b5764489d671661a88134a9
rex <rex@rex>
2018-04-03 19:22:49 +0300

    Added user controller, servise,impl

---
+++ TestTimeTable/src/main/java/org/itstep/service/UserService.java
@@ -0,0 +1,15 @@
+package org.itstep.service;
+
+import java.util.List;
+import org.itstep.model.Group;
+import org.itstep.model.User;
+
+public interface UserService {
+
+	User save (User user);
+	User update (User user);
+	User get (String login);
+	List<User> findAllByGroup(Group group);
+	void delete (String login);
+
+}

88d459115d001d2cfd983d94ba22d38fcfc776b9
Joseph Stalin <stalin@comunism.com>
2018-04-03 19:39:47 +0300

    /

--- TestTimeTable/src/test/java/org/itstep/service/LessonServiceTest.java
+++ TestTimeTable/src/test/java/org/itstep/service/LessonServiceTest.java
@@ -20,7 +20,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 public class LessonServiceTest {
 
 	@Autowired
-	SubjectService lessonService;
+	LessonService lessonService;
 
 	@Autowired
 	LessonDAO lessonDao;
